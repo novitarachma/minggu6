06806fe24fc658fe5c6b077404a57f8d76ffca29
novitarachma <novitarachma1811@gmail.com>
2021-10-15 00:25:20 +0700

    menambahkan fungsi hapus data (Bagian 5)

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -32,7 +32,12 @@
                                 <td>{{ $s->name }}</td>
                                 <td>{{ $s->class }}</td>
                                 <td>
-                                    <a href="/students/{{$s->id}}/edit" class="btn btn-warning">Edit</a>
+                                    <form action="/students/{{$s->id}}" method="post">
+                                        <a href="/students/{{$s->id}}/edit" class="btn btn-warning">Edit</a>
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" name="delete" class="btn btn-danger">Delete</button>
+                                    </form>
                                 </td>
                             </tr>
                             @endforeach
